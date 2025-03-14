8d845895f68b6f51bef94da9f1ded626c4698164
dh <hltd2003@gmail.com>
2025-03-14 10:30:51 +0700

    first commit

---
+++ routes/categories.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+let categorySchema = require('../schemas/category')
+
+router.get('/', async function(req, res, next) {
+    let categories = await categorySchema.find({});
+    res.send(categories);
+});
+
+router.get('/:id', async function(req, res, next) {
+    try {
+        let category = await categorySchema.findById(req.params.id);
+        res.send({
+            success:true,
+            data:category
+        });
+    } catch (error) {
+        res.status(404).send({
+            success:false,
+            message:error.message
+        })
+    }
+});
+
+router.post('/', async function(req, res, next) {
+    try {
+        let body = req.body;
+        let newCategory = categorySchema({
+            name:body.name
+        });
+        await newCategory.save()
+        res.status(200).send({
+            success:true,
+            data:newCategory
+        });
+    } catch (error) {
+        res.status(404).send({
+            success:false,
+            message:error.message
+        })
+    }
+});
+
+router.put('/:id', async function(req, res, next) {
+    try {
+        let body = req.body;
+        let updatedObj = {}
+        if(body.name){
+            updatedObj.name = body.name
+        }
+        await categorySchema.findByIdAndUpdate(req.params.id,updatedObj);
+        res.send({
+            success:true
+        });
+    } catch (error) {
+        res.status(404).send({
+            success:false,
+            message:error.message
+        })
+    }
+}); 
+
+router.delete('/:id', async function(req, res, next) {
+    try {
+        let updatedCategory =  await categorySchema.findByIdAndUpdate(req.params.id,{
+            isDeleted:true
+        },{new:true})
+        res.status(200).send({
+            success:true,
+            data:updatedCategory
+        });
+    } catch (error) {
+        res.status(404).send({
+            success:false,
+            message:error.message
+        })
+    }
+});
